34eb0a0b2e2e32ebd026bb06a5a851616efd716e
chenyuzuoo <zuocy1992@foxmail.com>
2017-01-20 19:30:15 +0100

    icons and basemap

---
+++ app/src/main/java/com/example/zuochenyu/myapplication/WelcomeActivity.java
@@ -0,0 +1,32 @@
+package com.example.zuochenyu.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(1200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+}
