8a962ecbd06366217f3acf7ddf2ef39c7e552147
chenyuzuoo <zuocy1992@foxmail.com>
2017-01-22 00:43:52 +0100

    colour layout,splash page

--- app/src/main/java/com/example/zuochenyu/myapplication/WelcomeActivity.java
+++ app/src/main/java/com/example/zuochenyu/myapplication/WelcomeActivity.java
@@ -3,12 +3,24 @@ package com.example.zuochenyu.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //set content view AFTER ABOVE sequence (to avoid crash)
+        this.setContentView(R.layout.activity_welcome);
+
         setContentView(R.layout.activity_welcome);
         Thread timerThread = new Thread() {
             public void run() {
